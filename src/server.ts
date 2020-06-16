1b6c3f2f45f6836f7947f49c050eeaf1e52c34d8
Ricky Almeida <ricky.almeida.dev@gmail.com>
2020-06-16 13:11:29 -0300

    add patient model

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,5 @@
-import { config } from 'dotenv';
+// import { config } from 'dotenv';
 import app from './app';
-
-config();
 
 app.listen(process.env.PORT || 5000, () => {
   console.log(`🌹 rose started on port ${process.env.PORT}`);
