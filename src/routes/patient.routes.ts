205b360164e5e71215a2fd9a8209fdb9af91bd8a
Pedro Siqueira <pedro.v.siqueira@gmail.com>
2020-06-17 16:20:18 -0300

    finish create user route

--- src/routes/patient.routes.ts
+++ src/routes/patient.routes.ts
@@ -4,4 +4,6 @@ import PatientController from '../controllers/patientController';
 const router = Router();
 const patientController = new PatientController();
 
-router.post('/signup/', patientController.create);
+router.post('/signup', patientController.create);
+
+export default router;
