04786f68c3cd20b9794a38e22c98850378c1e591
Fabricio Polato <fabriciopolato16@yahoo.com.br>
2020-06-18 12:49:42 -0300

    create-appointment method in controller created

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ import bodyParser from 'body-parser';
 import db from './database/connection';
 import patientRoutes from '../src/routes/patient.routes';
 import psychologistRoutes from '../src/routes/psychologist.routes';
+import appointmentsRoutes from '../src/routes/appointments.routes';
 
 const app = express();
 
@@ -12,7 +13,9 @@ db();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use('/patient', patientRoutes);
 app.use('/psychologist', psychologistRoutes);
+app.use('/appointments', appointmentsRoutes);
 
 export default app;
---
+++ src/controllers/appointmentController.ts
@@ -0,0 +1,63 @@
+import { Request, Response } from 'express';
+import Appointment from '../models/Appointment';
+
+interface Fail {
+  message: string;
+}
+
+interface Appointment {
+  _id: String;
+  startTime: Date;
+  endTime: Date;
+  psychologist: Object;
+}
+
+type FindResponse = Appointment | Fail;
+
+export default class AppointmentController {
+  public async create(
+    req: Request,
+    res: Response
+  ): Promise<Response<FindResponse>> {
+    const { startTime, endTime, psychologist } = req.body;
+
+    const appointment = {
+      startTime,
+      endTime,
+      psychologist,
+    };
+
+    if (!startTime || !endTime) {
+      return res.status(400).json({
+        message: 'Por favor, selecione um horário para o agendamento',
+      });
+    }
+
+    if (!psychologist) {
+      return res.status(500).json({ message: 'Erro mental do psicólogo' });
+    }
+
+    try {
+      const response = await Appointment.create(appointment);
+      return res.status(201).json(response);
+    } catch (error) {
+      console.log(error);
+      return res
+        .status(500)
+        .json({ message: 'Falha na criação do agendamento' });
+    }
+  }
+
+  public async find(
+    req: Request,
+    res: Response
+  ): Promise<Response<FindResponse>> {
+    try {
+      const response = await Appointment.find({});
+      return res.status(200).json(response);
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({ message: 'Agendamento não encontrado' });
+    }
+  }
+}
