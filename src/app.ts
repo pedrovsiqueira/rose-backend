d19ce13fdc427df1ec858b6cdc9720def0eeb21e
rickyalmeidadev <ricky.almeida.dev@gmail.com>
2020-08-16 02:43:49 -0300

    refactor: create an index file for all routes

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,10 @@
-import 'dotenv/config';
-
 import express from 'express';
 import bodyParser from 'body-parser';
 import logger from 'morgan';
 import cors from 'cors';
 
 import './database/connection';
-import patientRoutes from '../src/routes/patient.routes';
-import professionalRoutes from './routes/professional.routes';
-import appointmentsRoutes from '../src/routes/appointments.routes';
-import reviewsRoutes from '../src/routes/reviews.routes';
+import routes from './routes/index.routes';
 
 const app = express();
 
@@ -18,9 +13,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
-app.use('/patient', patientRoutes);
-app.use('/psychologist', professionalRoutes);
-app.use('/appointments', appointmentsRoutes);
-app.use('/reviews', reviewsRoutes);
+app.use(routes);
 
 export default app;
